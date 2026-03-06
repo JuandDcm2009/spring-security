4852d14f7e65a9c8f8dd10ec98658a05058cb403
Juan Diego C <juanchocristancho2009@gmail.com>
2026-03-05 19:33:24 -0500

    feat: :sparkles: Security 1

---
+++ src/main/java/com/ponscio/spring_security/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.ponscio.spring_security.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import lombok.RequiredArgsConstructor;
+
+@Configuration // Gestiona varios beans de configuracion
+@EnableWebSecurity 
+@RequiredArgsConstructor
+public class SecurityConfig {
+ 
+    @Bean 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+            .csrf(csrf -> csrf.disable()) // Hasta donde se es para solo trabajar seguridad basada en tokens
+                .authorizeHttpRequests(authRequest -> 
+                    authRequest
+                    .requestMatchers("/users/**").permitAll()
+                    .anyRequest().authenticated()
+                )
+            .formLogin(withDefaults())
+            .build();
+    }
+}
